4c3d4abf5fe2eef122bf93c559f3e47c20ba09b8
Yash728 <61221867+Yash728@users.noreply.github.com>
2020-08-22 17:51:35 +0530

    Updates

---
+++ precache-manifest.600c030a22e4e043fb2f5941111ab744.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93156baa39cc9634c506b882624a89ce",
+    "url": "/ReactJS/index.html"
+  },
+  {
+    "revision": "e3748ca8d46cab1a9345",
+    "url": "/ReactJS/static/css/main.01d8ff7f.chunk.css"
+  },
+  {
+    "revision": "a722c3ed3e55d509e108",
+    "url": "/ReactJS/static/js/2.13c488fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactJS/static/js/2.13c488fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3748ca8d46cab1a9345",
+    "url": "/ReactJS/static/js/main.d9a1d30d.chunk.js"
+  },
+  {
+    "revision": "8ccc12053a2895656788",
+    "url": "/ReactJS/static/js/runtime-main.6d50444f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactJS/static/media/logo.5d5d9eef.svg"
+  }
+]);
